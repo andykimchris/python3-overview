c218c1b1aa9cb832db80b5532d2cee349a88042d
andykimchris <andrew.muchiri97@gmail.com>
2021-09-22 20:13:32 +0300

    use of sets

---
+++ sets.py
@@ -0,0 +1,15 @@
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electric']}
+
+user_tag_1 = 'warm'
+user_tag_2 = 'exciting'
+user_tag_3 = 'electric'
+
+# Write your code below!
+tag_set = set(song_data['Retro Words'])
+tag_set.add(user_tag_1)
+tag_set.add(user_tag_2)
+tag_set.add(user_tag_3)
+
+song_data['Retro Words'] = tag_set
+
+print(song_data)
