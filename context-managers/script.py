c91357f75efadee7e6202df5e57f4e287c0ed344
andykimchris <andrew.muchiri97@gmail.com>
2021-09-23 22:25:21 +0300

    managed exceptions in class context manager

--- context-managers/script.py
+++ context-managers/script.py
@@ -10,9 +10,25 @@ class PoemFiles:
     self.opened_poem_file = open(self.file, self.mode)
     return self.opened_poem_file
 
-  def __exit__(self, *exc):
-    print('Closing poem file')
-    self.opened_poem_file.close()
+  def __exit__(self, exc_type, exc_value, traceback):
+    print(exc_type, exc_value, traceback, '\n')
+    # Write your code below:
+    if isinstance(exc_value, AttributeError):
+      self.opened_poem_file.close()
+      return True
+
 
 with PoemFiles('poem.txt', 'w') as open_poem_file:
    open_poem_file.write('Hope is the thing with feathers')
+
+
+
+with PoemFiles('poem.txt', 'r') as file:
+  print("---- Exception data below ---- \n ")
+  print(file.uppercasewords())
+
+with PoemFiles('poem.txt', 'r') as file2:
+  print(file2.read())
+  print(" \n ---- Exception data below ---- \n ")
+
+
