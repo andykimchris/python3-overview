de7595ed854615797a65e90f35bbc9743c9de149
andykimchris <andrew.muchiri97@gmail.com>
2021-09-24 14:09:05 +0300

    used contextlib decorator as manager

---
+++ context-managers/contextlib/script.py
@@ -0,0 +1,18 @@
+# Write your code below:
+from contextlib import contextmanager
+# class PoemFiles
+@contextmanager
+def poem_files(file, mode):
+  print('Opening File')
+  open_poem_file = open(file, mode)
+  try:
+    yield open_poem_file
+  finally:
+    print('Closing File')
+    open_poem_file.close()    
+
+with poem_files('poem.txt', 'a') as opened_file:
+ print('Inside yield')
+ opened_file.write('Rose is beautiful, Just like you.')
+
+
