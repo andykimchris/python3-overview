7ce004c0d6dce340b4027f409661e698a1d88c31
andykimchris <andrew.muchiri97@gmail.com>
2021-09-25 18:14:55 +0300

    removed unnecessary comments

--- sets/sets.py
+++ sets/sets.py
@@ -4,20 +4,17 @@ user_tag_1 = 'warm'
 user_tag_2 = 'exciting'
 user_tag_3 = 'electric'
 
-# Write your code below!
 tag_set = set(song_data['Retro Words'])
 tag_set.add(user_tag_1)
 tag_set.add(user_tag_2)
 tag_set.add(user_tag_3)
 
 song_data['Retro Words'] = tag_set
-
 print(song_data)
 
 
 song_data_users = {'Retro Words': ['pop', 'onion', 'warm', 'helloworld', 'happy', 'spam', 'electric']}
 
-# Write your code below!
 tag_set = set(song_data_users['Retro Words'])
 tag_set.discard('onion')
 tag_set.discard('helloworld')
@@ -31,25 +28,20 @@ print(song_data_users`)
 # For frozenset containers, we can only use the frozenset() constructor.
 music_tags = {'pop', 'warm', 'happy', 'electronic', 'synth', 'dance', 'upbeat'}
 
-# Checkpoint 1
 my_tags = frozenset(['pop', 'electronic', 'relaxing', 'slow', 'synth'])
 
-# Checkpoint 2
 # A union can be found using set or frozenset containers with the .union() method or | operator.
 frozen_tag_union = my_tags | music_tags
 print(frozen_tag_union)
 
-# Checkpoint 3
 # An intersection can be found using set or frozenset containers with the .intersection() method or & operator.
 regular_tag_intersect = music_tags & my_tags
 print(regular_tag_intersect)
 
-# Checkpoint 4
 # The difference can be found using set or frozenset containers with the .difference() method or - operator.
 frozen_tag_difference = my_tags - music_tags
 print(frozen_tag_difference)
 
-# Checkpoint 5
 # The symmetric difference can be found using set or frozenset containers with the .symmetric_difference() method or ^ operator.
 regular_tag_sd = music_tags ^ my_tags
 print(regular_tag_sd)
